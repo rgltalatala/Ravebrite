fdd75bb72a6a4adb6afd82d062b83ff6d33edcad
rgltalatala <rgltalatala@gmail.com>
2021-07-03 14:45:38 -0400

    Ali helped me again

--- frontend/ravebrite.jsx
+++ frontend/ravebrite.jsx
@@ -3,10 +3,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //Components
 import Root from './components/root';
-// import configureStore from './store/store';
+import { login, signup, logout} from './util/session_api_util';
+import configureStore from './store/store';
 
 
 document.addEventListener('DOMContentLoaded', () => {
+  let store = configureStore();
+  if (window.currentUser) {
+    const preloadedState = {
+      session: { id: window.currentUser.id },
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+
+  window.login = login
+  window.signup = signup
+  window.logout = logout
 
   const root = document.getElementById("root");
   ReactDOM.render(<Root />, root);
