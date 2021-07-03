c0109506a5c9ab1209b57c74ce38d84d35cae316
rgltalatala <rgltalatala@gmail.com>
2021-07-03 14:47:50 -0400

    Ali helped me once again

--- frontend/ravebrite.jsx
+++ frontend/ravebrite.jsx
@@ -8,19 +8,19 @@ import configureStore from './store/store';
 
 
 document.addEventListener('DOMContentLoaded', () => {
-  let store = configureStore();
-  if (window.currentUser) {
-    const preloadedState = {
-      session: { id: window.currentUser.id },
-      entities: {
-        users: { [window.currentUser.id]: window.currentUser }
-      }
-    };
-    store = configureStore(preloadedState);
-    delete window.currentUser;
-  } else {
-    store = configureStore();
-  }
+  // let store = configureStore();
+  // if (window.currentUser) {
+  //   const preloadedState = {
+  //     session: { id: window.currentUser.id },
+  //     entities: {
+  //       users: { [window.currentUser.id]: window.currentUser }
+  //     }
+  //   };
+  //   store = configureStore(preloadedState);
+  //   delete window.currentUser;
+  // } else {
+  //   store = configureStore();
+  // }
 
   window.login = login
   window.signup = signup
