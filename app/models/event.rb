ace3de0e34fc21b283b38a070f760973f4bbe03c
rgltalatala <rgltalatala@gmail.com>
2021-07-07 12:58:48 -0400

    Fix error to push to Heroku

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-    validates :title, :description, :location, :category, :start_date, :end_date, :start_time, :end_time, :presence: true
+    validates :title, :description, :location, :category, :start_date, :end_date, :start_time, :end_time, presence: true
     
     belongs_to :host,
         foreign_key: :user_id,
