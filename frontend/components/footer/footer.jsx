b47b077bae495af91585df3f3a94bfa9090e3213
rgltalatala <rgltalatala@gmail.com>
2021-07-16 01:11:46 -0400

    Finish footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 export const Footer = () => {
     return(
         <div className="footer-wrapper">
-            <p className="footer">Footer</p>
+            <div className="footer">
+                <Link to='https://www.linkedin.com/in/raphael-talatala-703943129/' >
+                    <i class="fab fa-linkedin-in footer-logo" ></i>
+                </Link>
+                <Link to='https://github.com/rgltalatala' >
+                    <i class="fab fa-github footer-logo"></i>
+                </Link>
+            </div>
         </div>
     )
 }
